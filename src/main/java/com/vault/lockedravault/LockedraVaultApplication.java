403d0e12c409dcb4b9b7f6723089e61a7adeba05
Marcell Biro <115219769+Marcel-zb96@users.noreply.github.com>
2025-01-15 18:43:30 +0100

    init-lockedra

---
+++ src/main/java/com/vault/lockedravault/LockedraVaultApplication.java
@@ -0,0 +1,13 @@
+package com.vault.lockedravault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockedraVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockedraVaultApplication.class, args);
+    }
+
+}
