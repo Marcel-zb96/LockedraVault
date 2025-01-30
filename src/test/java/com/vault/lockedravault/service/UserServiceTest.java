b42521d42493803545cfcdaa3145b8735bba1714
Marcell Biro <115219769+Marcel-zb96@users.noreply.github.com>
2025-01-30 14:38:13 +0100

    implement user service unit tests

---
+++ src/test/java/com/vault/lockedravault/service/UserServiceTest.java
@@ -0,0 +1,73 @@
+package com.vault.lockedravault.service;
+
+import com.vault.lockedravault.repository.UserRepository;
+import com.vault.lockedravault.security.model.SignUpRequest;
+import com.vault.lockedravault.security.model.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder encoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void createUser_happyCase() {
+        String testPassword = "testPassword";
+        String testEmail = "testEmail";
+        String testUserName = "testUserName";
+
+        SignUpRequest testSignUpRequest = new SignUpRequest(testUserName, testEmail, testPassword);
+        UserEntity testUserEntity = new UserEntity(testUserName, testEmail, testPassword);
+
+        when(encoder.encode(testPassword)).thenReturn(testPassword);
+        when(userRepository.save(testUserEntity)).thenReturn(testUserEntity);
+
+        userService.createUser(testSignUpRequest);
+
+        verify(userRepository, times(1)).save(testUserEntity);
+    }
+
+    @Test
+    void getUserEntity_userNameExists() {
+        String testUserName = "testUserName";
+        String testEmail = "testEmail";
+        String testPassword = "testPassword";
+
+        UserEntity testUserEntity = new UserEntity(testUserName, testEmail, testPassword);
+
+        when(userRepository.getUserEntityByUserName(testUserName)).thenReturn(Optional.of(testUserEntity));
+
+        Optional<UserEntity> result = userService.getUserEntity(testUserName);
+
+        assertTrue(result.isPresent());
+        assertThat(result).hasValue(testUserEntity);
+    }
+
+    @Test
+    void getUserEntity_userNameDoesNotExists() {
+        String testUserName = "testUserName";
+
+        when(userRepository.getUserEntityByUserName(testUserName)).thenReturn(Optional.empty());
+
+        Optional<UserEntity> result = userService.getUserEntity(testUserName);
+
+        assertTrue(result.isEmpty());
+    }
+}
